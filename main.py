5f83f24818f778813a8348193f0df3c5a8a468e9
zntb <zenetibi@yahoo.com>
2025-07-19 07:27:42 +0300

    URL Endpoints for All Data and Annual Data

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ stations = stations[["STAID", "STANAME                                 ", "CN" ]
 def home():
     return render_template("home.html", data=stations.to_html(classes='data', header="true", index=False))
 
+
 @app.route('/api/v1/<station>/<date>')
 def api(station, date):
     filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
@@ -35,5 +36,36 @@ def api(station, date):
         "date": date, 
         "temperature": temperature
     }
+ 
+    
+@app.route('/api/v1/<station>')
+def all_data(station):
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(
+        filename,
+        skiprows=20,
+        parse_dates=["    DATE"],
+        header=0
+    )
+    result = df.to_dict(orient='records')
+    return {
+        "station": station,
+        "data": result
+    }
+  
+    
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly_data(station, year):
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(
+        filename,
+        skiprows=20,
+        header=0
+    )
+    
+    df['    DATE'] = df['    DATE'].astype(str)  # Ensure DATE is string for filtering
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient='records')
+    return result
 
-app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
